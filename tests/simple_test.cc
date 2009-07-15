2eaebc50ef1f6536f231b575519b5f063dbcb993
Eric Jonas <jonas@mit.edu>
2009-07-15 09:17:16 -0400

    initial test framework for building.

---
+++ tests/simple_test.cc
@@ -0,0 +1,19 @@
+#include <boost/test/auto_unit_test.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/filesystem/fstream.hpp>
+#include <boost/format.hpp>
+
+using namespace boost;       
+
+
+BOOST_AUTO_TEST_SUITE(simpletest); 
+
+BOOST_AUTO_TEST_CASE(creation)
+{
+
+
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END(); 
