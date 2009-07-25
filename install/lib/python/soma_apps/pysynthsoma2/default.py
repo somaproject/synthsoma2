59dc0b2d27e26b5046d817e2656ff2189464dd8b
Eric Jonas <jonas@mit.edu>
2009-07-25 10:53:54 -0700

    New files for build system

---
+++ install/lib/python/soma_apps/pysynthsoma2/default.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+"""
+
+Default : Create the default instance
+
+
+
+"""
+import  pysynthsoma2
+import socket
+import time
+
+def ip(ipaddr):
+    """
+    Create default with IP connection
+
+    """
+
+    ne1 = pysynthsoma2.NetEventServer.createINet(ipaddr)    
+    nd1 = pysynthsoma2.NetDataServer.createINet(ipaddr)
+
+    return create_default(ne1, nd1)
+
+def domain(rootdir):
+
+    domaindir = tempfile.mkdtemp(prefix="soma")
+    ne1 = pysynthsoma2.NetEventServer.createDomain(rootdir)
+    nd1 = pysynthsoma2.NetDataServer.createDomain(domaindir)        
+
+    # create the missing sockets FIXME:
+    needed = ['dataretx', 'eventretx']
+    socks = []
+    for n in needed:
+        sock = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+        sock.bind(domaindir + "/" + n)
+        socks.append(sock)
+
+    return create_default(ne1, nd1)
+
+
+def create_default(ne1, nd1, TSPIKESOURCE_N = 8):
+
+    edm = pysynthsoma2.EventDeviceMap()
+    ddm = pysynthsoma2.DataDeviceMap()
+    
+    #create the timer 
+    td = pysynthsoma2.Timer()
+    edm[0] = td
+
+    # create the network event device
+        
+    edm[4] = ne1
+
+    for src in range(TSPIKESOURCE_N):
+        simplets = pysynthsoma2.SimpleTSpike()
+
+        # create the tspikes device
+        for j in range(10):
+            ts = pysynthsoma2.TSpike()
+            ts.src = 0;
+            ts.time = 50 * (j+1);
+            ts.x.wave = [1000 * j * i for i in range(32)]
+            ts.y.wave = [1000 * j * i for i in range(32)]
+            ts.a.wave = [1000 * j * i for i in range(32)]
+            ts.b.wave = [1000 * j * i for i in range(32)]
+
+
+            simplets.addTSpike(ts)
+        edm[8 + src] = simplets
+        ddm[0 + src] = simplets
+
+    event_bus = pysynthsoma2.EventBus(edm)
+    data_bus = pysynthsoma2.DataBus(ddm)
+
+    data_bus.setDataSink(nd1)
+
+    runner = pysynthsoma2.Runner(event_bus, data_bus)
+
+    return runner
